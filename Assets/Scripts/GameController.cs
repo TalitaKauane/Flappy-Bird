f2e14d8ac99b36e318658bfe6c7e3b828595d897
TalitaKauane <talitatklg@gmail.com>
2025-07-23 21:02:01 -0300

    code

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private GameObject message;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(message);
+
+            duck.SetActive(true);
+        }
     }
 }
